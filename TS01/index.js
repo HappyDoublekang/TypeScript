328cc791f2f69c274afa91f42636639070851e93
doublekk <467455219@qq.com>
2023-12-05 23:13:30 +0800

    feat:ts day 01

---
+++ TS01/index.js
@@ -0,0 +1,13 @@
+"use strict";
+let num = Infinity;
+let str = `${num}`;
+let b = true;
+let n = null;
+let un = undefined;
+// let v1:void = null
+let v2 = undefined;
+console.log(str);
+function myFn() {
+    return;
+    // return 123
+}
---
+++ TS01/index.ts
@@ -0,0 +1,15 @@
+let num:number = Infinity
+let str:string = `${num}`
+let b:boolean = true
+let n:null = null
+let un:undefined = undefined
+// let v1:void = null
+let v2:void = undefined
+
+console.log(str)
+
+
+function myFn():void {
+  return
+  // return 123
+}
